de1caebc23a390566c7662072630d4ddd490291e
v.borovkov <kurwa239056>
2016-09-22 15:36:15 +0300

    init 2

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -10,6 +10,9 @@ import android.view.MenuItem;
 
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+    public static Intent newIntent(Context context){
+        return new Intent(context,PhotoGalleryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -2,12 +2,16 @@ package com.bignerdranch.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.nfc.Tag;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -73,9 +77,35 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got old result!" + " lastResultId: " + lastResultId + " resultId : " + resultId);
         } else {
             Log.i(TAG, "Got new result!" + " lastResultId: " + lastResultId + " resultId : " + resultId);
+            Resources resources=getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi=PendingIntent.getActivity(this,0,i,0);
+
+            Notification notification=new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_picture_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_picture_title))
+                    .setContentText(resources.getString(R.string.new_picture_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManagerCompat notificationManager=NotificationManagerCompat.from(this);
+            notificationManager.notify(0,notification);
         }
+
+
+
+
         QueryPreferences.setLastResultId(this, resultId);
+
+
     }
+
+
+
+
+
+
 
     private boolean isNetwokAvailableAndConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
